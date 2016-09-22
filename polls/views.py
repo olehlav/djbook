b54c3772966d850efac7866fadf8fded91a5fb4d
Oleh Lav <olehlav@example.com>
2016-09-23 00:39:45 +0300

    add static and tests.py

--- polls/views.py
+++ polls/views.py
@@ -4,6 +4,8 @@ from .models import Question, Choice
 from django.template import RequestContext, loader
 from django.core.urlresolvers import reverse
 from django.views import generic
+from django.utils import timezone
+
 # Create your views here.
 
 def index_old(request):
@@ -58,11 +60,16 @@ class IndexView(generic.ListView):
 	context_object_name = 'latest_question_list'
 
 	def get_queryset(self):
-		return Question.objects.order_by('-pub_date')[:5]
+		return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 		
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
