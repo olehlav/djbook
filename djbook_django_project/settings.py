06d4b5b24aff4d902fc2b3c8f6bac6fc445210c7
Oleh Lav <olehlav@example.com>
2016-09-26 23:55:08 +0300

    add API for user and usergroup

--- djbook_django_project/settings.py
+++ djbook_django_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'polls',
+    'rest_framework'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -101,3 +102,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+    'PAGE_SIZE': 10
+}
